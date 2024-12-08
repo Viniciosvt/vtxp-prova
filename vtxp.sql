8cf33524b8e4b329fb010ebe173f2a26c91ec445
Viniciosvt <viniciosvteixeira@gmail.com>
2024-12-07 22:07:09 -0300

    banco-prova

--- vtxp.sql
+++ vtxp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/12/2024 às 00:35
+-- Tempo de geração: 08/12/2024 às 02:06
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -35,8 +35,18 @@ CREATE TABLE `fornecedor` (
   `telefone` varchar(150) NOT NULL,
   `categorias` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `foto` text NOT NULL
+  `foto` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `fornecedor`
+--
+
+INSERT INTO `fornecedor` (`id`, `nome`, `cpf_cnpj`, `endereco`, `telefone`, `categorias`, `email`, `foto`) VALUES
+(1, 'Machado', '21394812743', 'Rua Guilherme', '(32) 4239131321', 'Safadin', 'machado@gmail.com', 'c551c05c6a8c8a105e0d53d5bc709985.jpg'),
+(2, 'Vinicios', '3226616900', 'Rua Joao Caro 290', '(42) 981291042', 'Bosta lixo cuzinho', 'viniciosvteixeira@gmail.com', '2543f40215e9cd7b1b27a6be2b3d5ddb.jpg'),
+(3, 'Julia', '0924823947', 'Rua Visconde Bom Retiro ', '(42) marcha', 'Gostosinha meu amor', 'gopstosinah@gmail.com', 'images.jpg'),
+(4, '4328907329', 'Rua Curitiba', '(42) 423253431', 'Salve', 'safadabunduda@gmail.com', 'Luana', 'cenoura_250834906.jpg');
 
 -- --------------------------------------------------------
 
@@ -83,7 +93,7 @@ CREATE TABLE `sementes` (
   `dt_saida` date NOT NULL,
   `tipo_semente` varchar(150) NOT NULL,
   `fornecedor` varchar(150) NOT NULL,
-  `foto_semente` text NOT NULL
+  `foto_semente` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -91,7 +101,9 @@ CREATE TABLE `sementes` (
 --
 
 INSERT INTO `sementes` (`id`, `nome_semente`, `dt_entrada`, `dt_saida`, `tipo_semente`, `fornecedor`, `foto_semente`) VALUES
-(1, 'vinicios vaz teixeira', '2024-11-26', '0000-00-00', 'vinicios', 'vini@gmail.com', '');
+(1, 'vinicios vaz teixeira', '2024-11-26', '2024-12-19', 'vinicios', 'vini@gmail.com', 'ee0bd7afcb0d30e433c31d89c7a95b20.jpg'),
+(3, 'Abóbora', '2024-12-06', '2025-01-04', 'Cor alaranjada', 'Eduardo', 'eb495fc6c5a248d7e96c586ce8e1b4d2.jpg'),
+(5, 'a', '2024-12-20', '2024-12-20', 'a', 'a', 'cenoura_250834906.jpg');
 
 -- --------------------------------------------------------
 
@@ -112,7 +124,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `permissoes`) VALUES
-(1, 'vini', 'vini@gmail.com', '$2y$10$fxakdcp75Dntd9yA43OVg.XEoa/lTGCKhkLc2mrqxXZkta.vDxgaC', 'add,edit,del,super');
+(1, 'vini', 'vini@gmail.com', '$2y$10$vZa8G.6PpYQFWnM12nXEOe7rMq.6pMzvEFahIGe0EHOm0dvzRxHzu', 'add,edit,del,super'),
+(2, 'Machado', 'machado@gmail.com', '202cb962ac59075b964b07152d234b70', 'add,edit,del,super');
 
 --
 -- Índices para tabelas despejadas
@@ -156,7 +169,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `fornecedor`
 --
 ALTER TABLE `fornecedor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `foto_forncedor`
@@ -174,13 +187,13 @@ ALTER TABLE `foto_semente`
 -- AUTO_INCREMENT de tabela `sementes`
 --
 ALTER TABLE `sementes`
-  MODIFY `id` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
